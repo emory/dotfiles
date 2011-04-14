ef1ba25c058475c89a577bd624276bce36bd24b6
Emory Lundberg <emory@hellyeah.com>
2011-04-14 08:57:18 -0500

    yeah that's right

---
+++ bin/setup-github.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# set up github env
+
+git config --global user.name "Emory Lundberg"
+git config --global user.email "emory@hellyeah.com"
+git config --global github.user emory
+echo "get your API key and add it with: git config --global github.token 0123456789yourf0123456789token"
+
